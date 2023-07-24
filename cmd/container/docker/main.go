c74626ebd0c8e2cce7d17a5e9f32ae551f4d8a6b
Walter Angolar <walter.silva@tistech.co.ao>
2023-07-24 15:02:37 +0100

    feat: adicionei o Go flag para argumentos do cli docker

--- cmd/container/docker/main.go
+++ cmd/container/docker/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 
 	"github/angolarti/pluralcloud/pkg/container"
@@ -18,33 +19,40 @@ func main() {
 		panic(err)
 	}
 
-	const image = "docker.io/library/nginx"
+	image := flag.String("image", "alpine:latest", "Image name")
+	port := flag.Int("port", 0, "Port")
+	println(port)
+
+	// const image = "docker.io/library/nginx"
 	command := []string{"echo", "hello world"}
+	flag.Parse()
 
-	dc := container.NewDocker(ctx, cli, image, command)
-	dc.ImageName = image
+	println(flag.Args())
 
-	// resp, err := dc.Run()
+	dc := container.NewDocker(ctx, cli, *image, command)
+	dc.ImageName = *image
+	fmt.Println(dc.ImageName)
+	resp, err := dc.Run()
 	// containres, err := dc.List()
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// // fmt.Println("Container com ID", resp.ID)
-
-	// for _, container := range containres {
-	// 	fmt.Println("Container ID: ", container.ID, "Names: ", container.Names[0], "Status: ", container.Status)
-	// 	dc.Logs(container.ID)
-	// }
-
-	images, err := dc.ListAllImages()
 
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Container com ID", resp.ID)
 
-	for _, image := range images {
-		fmt.Println(image.ID)
-	}
+	// for _, container := range containres {
+	// 	fmt.Println("Container ID: ", container.ID, "Names: ", container.Names[0], "Status: ", container.Status)
+	// 	// dc.Logs(container.ID)
+	// }
+
+	// images, err := dc.ListAllImages()
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// for _, image := range images {
+	// 	fmt.Println(image.ID)
+	// }
 
 }
