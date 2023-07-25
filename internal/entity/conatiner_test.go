5a0679405577cdf3f299aa738dbf0a92624cf932
Lazaro Manuell <manuelvianacunga@gmail.com>
2023-07-25 19:12:59 +0100

    test: :white_check_mark: Add test for container entity

--- internal/entity/conatiner_test.go
+++ internal/entity/conatiner_test.go
@@ -1,1 +1,45 @@
 package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewContainer(t *testing.T) {
+	name := "my_container"
+	image := "my_image"
+	port := 8080
+	command := []string{"echo", "hello world"}
+
+	container := NewContainer(name, image, port, command)
+
+	assert.Equal(t, name, container.Name)
+	assert.Equal(t, image, container.Image)
+	assert.Equal(t, port, container.Port)
+	assert.Equal(t, command, container.Command)
+}
+
+func TestContainer_Validate(t *testing.T) {
+	name := "my_container"
+	image := "my_image"
+	port := 8080
+	command := []string{"echo", "hello world"}
+
+	container := NewContainer(name, image, port, command)
+
+	err := container.Validate()
+	assert.Nil(t, err)
+}
+
+func TestContainer_Validate_Invalid(t *testing.T) {
+	name := "my_container"
+	port := 8080
+	command := []string{"echo", "hello world"}
+
+	container := NewContainer(name, "", port, command)
+
+	err := container.Validate()
+	assert.NotNil(t, err)
+	assert.Equal(t, "image is required", err.Error())
+}
